2adba5341b18019ea3989ad233e99fa36cc0fc6c
Khalteck <khalidoyeneye@gmail.com>
2022-07-27 10:20:26 -0600

    added login and sign up pages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+AOS.init();
 //the header animation trigger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!S
 window.onscroll = function spread() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
@@ -13,6 +14,18 @@ window.onscroll = function spread() {
         document.getElementById("sign").style.display = "none";
     }
 }
+
+//to toggle password visibility!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function togglePsw() {
+    let passwordInput = document.querySelectorAll(".pass-input");
+    passwordInput.forEach((item, index) => {
+        if (item.type === "password") {
+            item.type = "text";
+        } else {
+            item.type = "password";
+        }
+    })
+  } 
 
 
 //the dropdown menu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -40,87 +53,6 @@ dropTray.addEventListener("click", () => {
     closeTray.style.display = "none";
     openTray.style.display = "block";
     navBar.style.backgroundColor = "rgba(0,0,0,0)";
-});
-
-//the sign up & sign in page state management!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//the home section
-const home = document.getElementById("home");
-//the second section
-const second = document.getElementById("second");
-//the third section
-const third = document.getElementById("third");
-//the third section
-const fourth = document.getElementById("fourth");
-//all login buttons
-const login = document.getElementById("trigger-login");
-//all sign up buttons
-const signup = document.getElementById("trigger-signup");
-login.addEventListener("click", () => {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.remove("hide")
-    document.getElementById("signup").classList.add("hide")
-});
-signup.addEventListener("click", () => {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.add("hide")
-    document.getElementById("signup").classList.remove("hide")
-});
-//to close the sign up page
-document.getElementById("close-login2").addEventListener("click", () => {
-    home.classList.remove("hide");
-    second.classList.remove("hide");
-    third.classList.remove("hide");
-    fourth.classList.remove("hide");
-    document.getElementById("login").classList.add("hide");
-    document.getElementById("signup").classList.add("hide");
-})
-//to close the ogin page
-document.getElementById("close-login1").addEventListener("click", () => {
-    home.classList.remove("hide");
-    second.classList.remove("hide");
-    third.classList.remove("hide");
-    fourth.classList.remove("hide");
-    document.getElementById("login").classList.add("hide");
-    document.getElementById("signup").classList.add("hide");
-})
-document.getElementById("trigger-login2").addEventListener("click", function() {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.remove("hide")
-    document.getElementById("signup").classList.add("hide")
-});
-document.getElementById("trigger-signup2").addEventListener("click", function() {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.add("hide")
-    document.getElementById("signup").classList.remove("hide")
-});
-//trigerring the mobile version
-document.getElementById("trigger-login-mob").addEventListener("click", () => {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.remove("hide")
-    document.getElementById("signup").classList.add("hide")
-});
-document.getElementById("trigger-signup-mob").addEventListener("click", () => {
-    home.classList.add("hide")
-    second.classList.add("hide")
-    third.classList.add("hide")
-    fourth.classList.add("hide")
-    document.getElementById("login").classList.add("hide")
-    document.getElementById("signup").classList.remove("hide")
 });
 
 //the section fade/slide in!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -158,11 +90,36 @@ window.addEventListener("load", function() {
         popupDownload.style.opacity = "1"
     }, 5000)
 });
-
-AOS.init();
-
-
 document.getElementById("close-popup").addEventListener("click", () => {
     popupDownload.classList.add("hide")
     document.getElementById("download-sec").classList.add("hide");
 });
+
+/*to submit signup data!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+function sendData() {
+    let signupData = new FormData(document.getElementById("signupForm"));
+    fetch("#", {
+      method: "POST",
+      body: JSON.stringify(signupData)
+    })
+   //return server response as text
+    .then((result) => {
+      if (result.status != 200) { throw new Error("Bad Server Response"); }
+      return result.text();
+    })
+    //server response
+    .then((response) => {
+      console.log(response);
+      if (response.error) {
+        alert("Invalid details"); /*displays error message*/
+      } else {
+        window.open(
+          "target.html"
+        ); /*opens the target page while Id & password matches*/
+      }
+    })
+    //handle errors
+    .catch((error) => { console.log(error); });
+  }
+
+  /*to login!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
